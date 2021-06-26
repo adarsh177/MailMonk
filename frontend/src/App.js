5ece7161ff1390af32dbb7054d6e41c1f7f7338d
Pratik-Kumar-621 <70286186+Pratik-Kumar-621@users.noreply.github.com>
2021-06-26 23:31:46 +0530

    first commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,24 @@
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+import React from "react";
 
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./Screens/Home/home";
 import Newdirectmail from "./Screens/Newdirectmail/newdirectmail";
-
+import History from "./Screens/Mail History/history";
 
 function App() {
   return (
     <Router>
-
       <Switch>
-
         <Route exact path="/">
-        <Home/>
+          <Home />
         </Route>
         <Route exact path="/newdirectmail">
-        <Newdirectmail/>
+          <Newdirectmail />
         </Route>
-
+        <Route exact path="/history">
+          <History />
+        </Route>
       </Switch>
-
-
     </Router>
   );
 }
